6b5df995fb2095cc9a91dd6ec261de06996229a7
Павел Литвиненко <digitaldistortion@ya.ru>
2018-06-06 15:06:16 +0300

    no message

--- lib/logstash/inputs/mongodb.rb
+++ lib/logstash/inputs/mongodb.rb
@@ -171,7 +171,7 @@ class LogStash::Inputs::MongoDB < LogStash::Inputs::Base
     require "sequel"
     require "rufus/scheduler"
     placeholder_db_path = File.join(@placeholder_db_dir, @placeholder_db_name)
-    conn = Mongo::Client.new(@uri, :connect => :direct, { slave_ok: true })
+    conn = Mongo::Client.new(@uri)
 
     @host = Socket.gethostname
     @logger.info("Registering MongoDB input")
